32b7cf8bafdd9b40fb582faeb9394d6242fffb40
Ayush <ayush.agarwal.ggn@gmail.com>
2021-04-18 19:43:52 +0530

    Blog count added

--- home/views.py
+++ home/views.py
@@ -61,7 +61,6 @@ def item(request,slug):
             replydict[reply.parent.sno]=[reply]
         else:
             replydict[reply.parent.sno].append(reply)
-
     context={'item':item,'comments':comments,'user':request.user,'replydict':replydict}
     return render(request,'home/item.html',context)
 
